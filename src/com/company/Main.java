e5001427215844c3f3a4833c89ab958bbb54a36a
Nikita Smorschok <nsmorschok@gmail.ru>
2022-05-12 20:31:54 +0300

    Data_dz_ls15

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,22 +5,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите дату: ");// введите дату последнего вторника:
+
         GregorianCalendar calendar = new GregorianCalendar();
         Scanner scan = new Scanner(System.in);
-
+        System.out.println("Введите год: ");// введите дату последнего вторника:
         int year = scan.nextInt();
         calendar.set(Calendar.YEAR, year);
-
+        System.out.println("Введите месяц: ");
         int month = scan.nextInt();
         calendar.set(Calendar.MONTH, month - 1);
-
+        System.out.println("Введите дунь: ");
         int day = scan.nextInt();
         calendar.set(Calendar.DAY_OF_MONTH, day); // +7 для вывода следуюзего вторника
 
         DateFormat df = new SimpleDateFormat("yyy MMM dd EEEE");
         System.out.println(df.format(calendar.getTime()));
-
     }
 }
 
